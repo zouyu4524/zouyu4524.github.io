00cf05f6259404f362a9a87f0272d6864584c043
zouyu4524 <920789718@qq.com>
2019-12-20 16:09:47 +0800

    feat: add back to top button

--- _includes/scripts/page.js
+++ _includes/scripts/page.js
@@ -1,3 +1,21 @@
-/*(function () {
-
-})();*/
+(function () {
+  var SOURCES = window.TEXT_VARIABLES.sources;
+  window.Lazyload.js(SOURCES.jquery, function() {
+    $("#back-top").hide();
+    $(document).ready(function () {
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+          $('#back-top').fadeIn();
+        } else {
+          $('#back-top').fadeOut();
+        }
+      });
+      $('#back-top a').click(function () {
+        $('body,html').animate({
+          scrollTop: 0
+        }, 800);
+          return false;
+      });
+    });
+  });
+})();
